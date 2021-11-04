ca3f356ac7df317fe1e84cbe8556a13af229c40f
Marius Tirnacop <89193936+MariusTirnacop@users.noreply.github.com>
2021-11-04 02:56:06 +0200

    Delete weather-app directory

--- weather-app/api/endpoints.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = "3de5c524c895783b53b14475fb8eb9ae";
-
-function getCurrentWeatherEndpoint(city) {
-  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
-}
-
-function getForecastEndpoint(city) {
-  return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=ro&units=metric&appid=${API_KEY}`;
-}
